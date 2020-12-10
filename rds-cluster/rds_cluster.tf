af023289393e3793c027fcdc6641a24ef6484fa3
ortacu <72772410+ortacu@users.noreply.github.com>
2020-12-10 04:20:28 -0600

    Update rds_cluster.tf

--- rds-cluster/rds_cluster.tf
+++ rds-cluster/rds_cluster.tf
@@ -1,1 +1,35 @@
+module "db" {
+source = "ortacu/rds-cluster/aws"
+region = "us-east-2"
+subnet_ids = [
+"subnet-xxxxxxxxx", 
+"subnet-xxxxxxxxx", 
+"subnet-xxxxxxxxx"
+]
+security_group_name = "db"
+allowed_hosts = [
+"0.0.0.0/0"
+]
+db_name = "dbname"
+engine = "aurora"
+engine_version = "5.6.10a"
+instance_class = "db.t2.micro"
+username = "foo"
+password = "foobarbaz"
+publicly_accessible = true
+}
 
+
+
+output "region" {
+value = "${module.db.region}"
+}
+output "subnet_list" {
+value = "${module.db.subnet_list}"
+}
+output "allows_hosts" {
+value = "${module.db.allowed_hosts}"
+}
+output "DB_NAME" {
+value = "${module.db.DB_NAME}"
+}
